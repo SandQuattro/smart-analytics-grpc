c85f7dbcea0c88bfc57c4a39a2f95e6719614883
sandquattro <sandquattro@gmail.com>
2024-06-12 13:49:39 +0300

    added CreateThread

--- smart-analytics/assistants/grpc/assistants_grpc.pb.go
+++ smart-analytics/assistants/grpc/assistants_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -22,6 +23,7 @@ const (
 	AssistantService_ListAssistantFiles_FullMethodName  = "/assistants.AssistantService/ListAssistantFiles"
 	AssistantService_DeleteAssistantFile_FullMethodName = "/assistants.AssistantService/DeleteAssistantFile"
 	AssistantService_LinkFileToAssistant_FullMethodName = "/assistants.AssistantService/LinkFileToAssistant"
+	AssistantService_CreateThread_FullMethodName        = "/assistants.AssistantService/CreateThread"
 )
 
 // AssistantServiceClient is the client API for AssistantService service.
@@ -31,6 +33,7 @@ type AssistantServiceClient interface {
 	ListAssistantFiles(ctx context.Context, in *AssistantRequest, opts ...grpc.CallOption) (*ListAssistantFilesResponse, error)
 	DeleteAssistantFile(ctx context.Context, in *AssistantFileRequest, opts ...grpc.CallOption) (*DeletedObject, error)
 	LinkFileToAssistant(ctx context.Context, in *AssistantFileRequest, opts ...grpc.CallOption) (*AssistantFileData, error)
+	CreateThread(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*AssistantObject, error)
 }
 
 type assistantServiceClient struct {
@@ -68,6 +71,15 @@ func (c *assistantServiceClient) LinkFileToAssistant(ctx context.Context, in *As
 	return out, nil
 }
 
+func (c *assistantServiceClient) CreateThread(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*AssistantObject, error) {
+	out := new(AssistantObject)
+	err := c.cc.Invoke(ctx, AssistantService_CreateThread_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AssistantServiceServer is the server API for AssistantService service.
 // All implementations must embed UnimplementedAssistantServiceServer
 // for forward compatibility
@@ -75,6 +87,7 @@ type AssistantServiceServer interface {
 	ListAssistantFiles(context.Context, *AssistantRequest) (*ListAssistantFilesResponse, error)
 	DeleteAssistantFile(context.Context, *AssistantFileRequest) (*DeletedObject, error)
 	LinkFileToAssistant(context.Context, *AssistantFileRequest) (*AssistantFileData, error)
+	CreateThread(context.Context, *emptypb.Empty) (*AssistantObject, error)
 	mustEmbedUnimplementedAssistantServiceServer()
 }
 
@@ -90,6 +103,9 @@ func (UnimplementedAssistantServiceServer) DeleteAssistantFile(context.Context, 
 }
 func (UnimplementedAssistantServiceServer) LinkFileToAssistant(context.Context, *AssistantFileRequest) (*AssistantFileData, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method LinkFileToAssistant not implemented")
+}
+func (UnimplementedAssistantServiceServer) CreateThread(context.Context, *emptypb.Empty) (*AssistantObject, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateThread not implemented")
 }
 func (UnimplementedAssistantServiceServer) mustEmbedUnimplementedAssistantServiceServer() {}
 
@@ -158,6 +174,24 @@ func _AssistantService_LinkFileToAssistant_Handler(srv interface{}, ctx context.
 	return interceptor(ctx, in, info, handler)
 }
 
+func _AssistantService_CreateThread_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AssistantServiceServer).CreateThread(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: AssistantService_CreateThread_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AssistantServiceServer).CreateThread(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // AssistantService_ServiceDesc is the grpc.ServiceDesc for AssistantService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -176,6 +210,10 @@ var AssistantService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "LinkFileToAssistant",
 			Handler:    _AssistantService_LinkFileToAssistant_Handler,
+		},
+		{
+			MethodName: "CreateThread",
+			Handler:    _AssistantService_CreateThread_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
